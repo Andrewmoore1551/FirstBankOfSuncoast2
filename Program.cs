85c163a926d565833e8eb2830d6aff6a2d84b3e3
Andrew Moore <mooreandrew015@gmail.com>
2021-04-23 10:47:56 -0400

    created a class transactions and added csvHelper

--- Program.cs
+++ Program.cs
@@ -1,12 +1,34 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using CsvHelper;
 
 namespace FirstBankOfSuncoast2
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Transaction
     {
-      Console.WriteLine("Welcome to C#");
+        public string Type { get; set; }
+        public string Account { get; set; }
+        public int Amount { get; set; }
+        public DateTime TimeStamp { get; set; }
     }
-  }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Welcome to First Bank Of Suncoast");
+            Console.WriteLine();
+
+            var fileReader = new StreamReader("transactions.csv");
+            var csvReader = new CsvReader(fileReader, CultureInfo.InvariantCulture);
+            var transactions = csvReader.GetRecords<Transaction>().ToList();
+            fileReader.Close();
+
+
+        }
+    }
 }
